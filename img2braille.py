d6b5c224296757d8560491e546c2ea9caf7ed78b
Joshua Steiner <josh.steiner@protonmail.com>
2018-01-25 18:09:51 +0100

    improve algorithm

--- img2braille.py
+++ img2braille.py
@@ -2,18 +2,22 @@ from PIL import Image, ImageOps, ImageEnhance
 
 white_threshold = 128
 
+def is_black(pixel):
+    return pixel < white_threshold
+
+
 def dither(pixels, dims):
     (width, height) = dims
     for i in range(height-1):
         for j in range(1, width-1):
-            old = pixels[i][j] 
-            new = 255 if old > white_threshold else 0
-            err = old - new 
+            old = pixels[i][j]
+            new = 0 if is_black(old) else 255
+            err = old - new
             pixels[ i ][ j ]  = new
             pixels[i+1][ j ] += err * 7/16
-            pixels[i-1][j+1] += err * 3/16 
-            pixels[ i ][j+1] += err * 5/16 
-            pixels[i+1][j+1] += err * 1/16 
+            pixels[i-1][j+1] += err * 3/16
+            pixels[ i ][j+1] += err * 5/16
+            pixels[i+1][j+1] += err * 1/16
     return pixels
 
 
@@ -25,27 +29,29 @@ def toBraille(pixels, dims):
         braille.append([])
         for j in range(0, width, 2):
             offset = 0
-            try:
-                offset += (pixels[ i ][ j ] < white_threshold) * 2 ** 0 + (pixels[ i ][j+1] < white_threshold) * 2 ** 3
-                offset += (pixels[i+1][ j ] < white_threshold) * 2 ** 1 + (pixels[i+1][j+1] < white_threshold) * 2 ** 4
-                offset += (pixels[i+2][ j ] < white_threshold) * 2 ** 2 + (pixels[i+2][j+1] < white_threshold) * 2 ** 5
-                offset += (pixels[i+3][ j ] < white_threshold) * 2 ** 6 + (pixels[i+3][j+1] < white_threshold) * 2 ** 7
-            except IndexError:
-                pass 
+            for k in range(min(4, height - i)):
+                offset += is_black(pixels[i+k][j]) * 2 ** k 
+                offset += is_black(pixels[i+k][j+1]) * 2 ** (k+3)
+                if k == 4:
+                    offset += is_black(pixels[i+k][j]) * 2 ** 6
+                    offset += is_black(pixels[i+k][j+1]) * 2 ** 7
             c = chr(0x2800 + offset)
-            braille[-1].append(c) 
+            braille[-1].append(c)
     return braille
 
 
 def img2braille(filename):
     with Image.open(filename) as img:
-        img.thumbnail((400,400))
+        #img.thumbnail((400, 400))
         pixels = []
         (width, height) = img.size
-        enhancer = ImageEnhance.Contrast(img)
-        img = enhancer.enhance(1)
+        try:
+            img = ImageEnhance.Contrast(img).enhance(1)
+            img = ImageEnhance.Brightness(img).enhance(1)
+        except ValueError:
+            pass
         for i, px in enumerate(ImageOps.grayscale(img).getdata()):
-            if (i % width == 0):
+            if i % width == 0:
                 pixels.append([])
             pixels[-1].append(px)
         dither(pixels, img.size)
